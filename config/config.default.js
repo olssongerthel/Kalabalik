eaada647dec6514543cc168ece18d825e0d177b5
Adam Gerthel <insats@gmail.com>
2015-10-24 14:55:18 +0200

    Improved default config docs

--- config/config.default.js
+++ config/config.default.js
@@ -2,25 +2,37 @@ var sql = require('mssql');
 
 // Add mssql database config here as per instructions from:
 // https://www.npmjs.com/package/mssql
-// Invoicing is the Avance Fakturering database (usually 'Fakt000')
-// and Supplier should be the Avance Leverantör database (usually 'lres000')
+// 'invoicing' is the Avance Fakturering database (usually 'Fakt000')
+// and 'supplier' should be the Avance Leverantör database (usually 'lres000')
 
 var invoicing = {
-
+  // user: '',
+  // password: '',
+  // server: '',
+  // database: '',
+  // options: {
+  //     encrypt: true
+  // }
 }
 
 var supplier = {
-
+  // user: '',
+  // password: '',
+  // server: '',
+  // database: '',
+  // options: {
+  //     encrypt: true
+  // }
 }
 
-// Add a username and password to enable basic authentication
+// Add a username and password to enable basic authentication for the API.
 var credentials = {
   username: '',
   password: ''
 }
 
+exports.port = 3000; // Select the port to run Kalabalik on.
 exports.sql = sql;
 exports.invoicing = invoicing;
 exports.supplier = supplier;
 exports.credentials = credentials;
-exports.port = 3000;
