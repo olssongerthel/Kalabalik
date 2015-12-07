40e8fbe92de6d913408c6b1d3d046fb8a33c7321
Adam Gerthel <insats@gmail.com>
2015-12-07 20:17:44 +0100

    Only use endpoints that are available

--- server.js
+++ server.js
@@ -39,138 +39,158 @@ if (settings.credentials.username) {
 // Use json bodyparser for PUT and POST requests.
 app.use(bodyParser.json());
 
+/**
+ * Generates the front page welcome message and endpoints list.
+ */
 var welcome = function(req, res) {
+
   var message = 'Kalabalik is up and running.';
+  // Create an array that we can store all endpoints in
+  var endPoints = [];
   if (req) {
     var baseUrl = req.protocol + '://' + req.get('host');
+
+    // Invoicing endpoints
+    if (settings.mssql.databases.invoicing) {
+      endPoints.push({
+        name: 'Orders',
+        url: baseUrl + '/orders',
+        type: 'GET'
+      },
+      {
+        name: 'Order',
+        url: baseUrl + '/orders/:ORDER-ID',
+        type: 'GET, PUT'
+      },
+      {
+        name: 'Order history',
+        url: baseUrl + '/order-history',
+        type: 'GET'
+      },
+      {
+        name: 'Order history order',
+        url: baseUrl + '/order-history/:ORDER-ID',
+        type: 'GET'
+      },
+      {
+        name: 'Line items',
+        url: baseUrl + '/line-items',
+        type: 'GET'
+      },
+      {
+        name: 'Line items per order',
+        url: baseUrl + '/line-items/:ORDER-ID',
+        type: 'GET'
+      },
+      {
+        name: 'Line item',
+        url: baseUrl + '/line-items/:ORDER-ID/:ROW',
+        type: 'GET, PUT'
+      },
+      {
+        name: 'Customers',
+        url: baseUrl + '/customers',
+        type: 'GET'
+      },
+      {
+        name: 'Customer',
+        url: baseUrl + '/customers/:CUSTOMER-ID',
+        type: 'GET, PUT'
+      },
+      {
+        name: 'Products',
+        url: baseUrl + '/products',
+        type: 'GET'
+      },
+      {
+        name: 'Product',
+        url: baseUrl + '/products/:SKU',
+        type: 'GET'
+      },
+      {
+        name: 'Stock status',
+        url: baseUrl + '/stock',
+        type: 'GET'
+      });
+    };
+
+    // Supplier endpoints
+    if (settings.mssql.databases.supplier) {
+      endPoints.push({
+        name: 'Suppliers',
+        url: baseUrl + '/suppliers',
+        type: 'GET'
+      },
+      {
+        name: 'Purchase orders',
+        url: baseUrl + '/purchase-orders',
+        type: 'GET'
+      },
+      {
+        name: 'Purchase order',
+        url: baseUrl + '/purchase-orders/:ORDER-ID',
+        type: 'GET, PUT'
+      },
+      {
+        name: 'Purchase order line items',
+        url: baseUrl + '/purchase-order-line-items',
+        type: 'GET'
+      },
+      {
+        name: 'Purchase order line items per order',
+        url: baseUrl + '/purchase-order-line-items/:ORDER-ID',
+        type: 'GET'
+      },
+      {
+        name: 'Purchase order line item',
+        url: baseUrl + '/purchase-order-line-items/:ORDER-ID/:ROW',
+        type: 'GET, PUT'
+      });
+    };
+
     res.jsonp({
       message: message,
-      endPoints: [
-        {
-          name: 'Orders',
-          url: baseUrl + '/orders',
-          type: 'GET'
-        },
-        {
-          name: 'Order',
-          url: baseUrl + '/orders/:ORDER-ID',
-          type: 'GET, PUT'
-        },
-        {
-          name: 'Order history',
-          url: baseUrl + '/order-history',
-          type: 'GET'
-        },
-        {
-          name: 'Order history order',
-          url: baseUrl + '/order-history/:ORDER-ID',
-          type: 'GET'
-        },
-        {
-          name: 'Line items',
-          url: baseUrl + '/line-items',
-          type: 'GET'
-        },
-        {
-          name: 'Line items per order',
-          url: baseUrl + '/line-items/:ORDER-ID',
-          type: 'GET'
-        },
-        {
-          name: 'Line item',
-          url: baseUrl + '/line-items/:ORDER-ID/:ROW',
-          type: 'GET, PUT'
-        },
-        {
-          name: 'Customers',
-          url: baseUrl + '/customers',
-          type: 'GET'
-        },
-        {
-          name: 'Customer',
-          url: baseUrl + '/customers/:CUSTOMER-ID',
-          type: 'GET, PUT'
-        },
-        {
-          name: 'Products',
-          url: baseUrl + '/products',
-          type: 'GET'
-        },
-        {
-          name: 'Product',
-          url: baseUrl + '/products/:SKU',
-          type: 'GET'
-        },
-        {
-          name: 'Stock status',
-          url: baseUrl + '/stock',
-          type: 'GET'
-        },
-        {
-          name: 'Suppliers',
-          url: baseUrl + '/suppliers',
-          type: 'GET'
-        },
-        {
-          name: 'Purchase orders',
-          url: baseUrl + '/purchase-orders',
-          type: 'GET'
-        },
-        {
-          name: 'Purchase order',
-          url: baseUrl + '/purchase-orders/:ORDER-ID',
-          type: 'GET, PUT'
-        },
-        {
-          name: 'Purchase order line items',
-          url: baseUrl + '/purchase-order-line-items',
-          type: 'GET'
-        },
-        {
-          name: 'Purchase order line items per order',
-          url: baseUrl + '/purchase-order-line-items/:ORDER-ID',
-          type: 'GET'
-        },
-        {
-          name: 'Purchase order line item',
-          url: baseUrl + '/purchase-order-line-items/:ORDER-ID/:ROW',
-          type: 'GET, PUT'
-        }
-      ]
+      endPoints: endPoints
     });
   } else {
     return message;
   }
 };
 
-app.get('/orders', orders.index);
-app.route('/orders/:id')
-  .get(orders.findById)
-  .put(orders.update);
-app.get('/order-history', orderHistory.index);
-app.get('/order-history/:id', orderHistory.findById);
-app.get('/line-items', lineItems.index);
-app.get('/line-items/:order', lineItems.findByOrder);
-app.route('/line-items/:order/:row')
-  .get(lineItems.findByOrderAndRow)
-  .put(lineItems.update);
-app.get('/customers', customers.index);
-app.route('/customers/:id')
-  .get(customers.findById)
-  .put(customers.update);
-app.get('/products', products.index);
-app.get('/products/:sku', products.findBySKU);
-app.get('/stock', stock.index);
-app.get('/suppliers', suppliers.index);
-app.get('/purchase-orders', purchaseOrders.index);
-app.route('/purchase-orders/:id')
-  .get(purchaseOrders.findById)
-  .put(purchaseOrders.update);
-app.get('/purchase-order-line-items', purchaseOrderLineItems.index);
-app.get('/purchase-order-line-items/:order', purchaseOrderLineItems.findByOrder);
-app.route('/purchase-order-line-items/:order/:row')
-  .get(purchaseOrderLineItems.findByOrderAndRow)
-  .put(purchaseOrderLineItems.update);
+if (settings.mssql.databases.invoicing) {
+  app.get('/orders', orders.index);
+  app.route('/orders/:id')
+    .get(orders.findById)
+    .put(orders.update);
+  app.get('/order-history', orderHistory.index);
+  app.get('/order-history/:id', orderHistory.findById);
+  app.get('/line-items', lineItems.index);
+  app.get('/line-items/:order', lineItems.findByOrder);
+  app.route('/line-items/:order/:row')
+    .get(lineItems.findByOrderAndRow)
+    .put(lineItems.update);
+  app.get('/customers', customers.index);
+  app.route('/customers/:id')
+    .get(customers.findById)
+    .put(customers.update);
+  app.get('/products', products.index);
+  app.get('/products/:sku', products.findBySKU);
+  app.get('/stock', stock.index);
+}
+
+if (settings.mssql.databases.supplier) {
+  app.get('/suppliers', suppliers.index);
+  app.get('/purchase-orders', purchaseOrders.index);
+  app.route('/purchase-orders/:id')
+    .get(purchaseOrders.findById)
+    .put(purchaseOrders.update);
+  app.get('/purchase-order-line-items', purchaseOrderLineItems.index);
+  app.get('/purchase-order-line-items/:order', purchaseOrderLineItems.findByOrder);
+  app.route('/purchase-order-line-items/:order/:row')
+    .get(purchaseOrderLineItems.findByOrderAndRow)
+    .put(purchaseOrderLineItems.update);
+}
+
 app.get('', welcome);
 
 // Initiate server
