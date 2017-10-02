aa45900e3cf343fb7c149ca79541c3ca326c9614
Adam Gerthel <insats@gmail.com>
2017-10-02 11:41:20 +0200

    Changed report description key name

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ var welcome = function(req, res) {
       endPoints.push({
         name: 'Reports',
         type: 'GET',
-        urls: buildReportUrls(list)
+        endpoints: buildReportUrls(list)
       });
     }
 
